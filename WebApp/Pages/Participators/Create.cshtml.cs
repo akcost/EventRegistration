345c8d6d2b76b310f0b9c1add35ae14ac6c59279
akcost <akselcosta@gmail.com>
2023-07-11 20:49:48 +0300

    Improved Participator form to add a Company or a Person as a Participator

--- WebApp/Pages/Participators/Create.cshtml.cs
+++ WebApp/Pages/Participators/Create.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -21,24 +22,50 @@ namespace WebApp.Pages.Participators
 
         public IActionResult OnGet()
         {
-        ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "CompanyName");
-        ViewData["PersonId"] = new SelectList(_context.Persons, "Id", "PersonFirstName");
+            ViewData["PaymentType"] = new SelectList(_context.Companies, "Id", "CompanyName");
+            ViewData["PersonId"] = new SelectList(_context.Persons, "Id", "PersonFirstName");
             return Page();
         }
 
-        [BindProperty]
-        public Participator Participator { get; set; } = default!;
-        
+        // [BindProperty] public Participator Participator { get; set; } = default!;
+        [BindProperty] public Person Person { get; set; } = default!;
+        [BindProperty] public Company Company { get; set; } = default!;
+        [BindProperty] public PaymentType PaymentType { get; set; }
+
+        public Boolean IsPerson { get; set; } = true;
+
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.Participators == null || Participator == null)
-            {
-                return Page();
-            }
 
-            _context.Participators.Add(Participator);
+            // Check if Person info was submitted or Company info
+            if (Person?.PersonFirstName == null)
+            {
+                _context.Companies.Add(Company!);
+                await _context.SaveChangesAsync();
+
+                Participator participator = new Participator()
+                {
+                    Company = Company,
+                    CompanyId = Company!.Id,
+                    PaymentType = PaymentType,
+                };
+                _context.Participators.Add(participator);
+            }
+            else
+            {
+                _context.Persons.Add(Person);
+                await _context.SaveChangesAsync();
+
+                Participator participator = new Participator()
+                {
+                    Person = Person,
+                    PersonId = Person.Id,
+                    PaymentType = PaymentType,
+                };
+                _context.Participators.Add(participator);
+            }
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
